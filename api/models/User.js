331c17bea0100cb08303058938ea06329c346675
charles <chuckhperry@gmail.com>
2020-12-14 15:35:51 -0800

    added icon to UserSchema

--- api/models/User.js
+++ api/models/User.js
@@ -6,6 +6,7 @@ const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     username: {type: String, required: true},
+    icon: {type: Number},
     interests: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Interests'
