748f3146ddf6e76dd7eac2c8be67cc131549fe88
Matthew Coalson <matthew.coalson@gmail.com>
2020-12-14 14:50:21 -0600

    Cleans user model

--- api/models/User.js
+++ api/models/User.js
@@ -1,27 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    username: {type: String, required: true},
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
     interests: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Interests'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Interests",
     },
     upcomingKickoffs: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Kickoffs'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Kickoffs",
     },
     pastKickoffs: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Kickoffs'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Kickoffs",
     },
     friends: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
-}, {timestamps: true})
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.Model('Model', ModelSchema)
+module.exports = mongoose.Model("Model", ModelSchema);
